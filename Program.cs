5f7961fa296b8e38b38d7c3d00b115fa2a156d74
Alexandr <jwchaganav@gmail.com>
2022-10-18 11:46:53 +0400

    end

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿// Вывод третьей цифры заданного числа
+int max = 1000000000;
+int num = ReadInt($"Введите любое число меньше {max}: ");
+if (num > max)
+{
+    num = ReadInt($"Вы ввели слишком большое число, попробуйте еще раз");
+}
+while(num/max == 0)
+{
+    max = max/10;
+}
 
-Console.WriteLine("Hello, World!");
+if (num > 99)
+{
+    int second = num/(max/10)*10;
+    int temp = num/(max/100);
+    int three = temp-second;
+    Console.WriteLine($"{second}");
+    Console.WriteLine($"В числе {num} третья цифра: {three}");
+}
+else
+{
+    Console.WriteLine($"Sorry, в числе {num} третьей цифры нет!");
+}
+// Metods
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
